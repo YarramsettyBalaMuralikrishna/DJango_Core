8177d246ba40dfbfc9d72c65bf357e2898245e9a
BALA MURALI KRISHNA YARRAMSETTY <98406932+YarramsettyBalaMuralikrishna@users.noreply.github.com>
2025-01-27 20:06:25 +0530

    get,post methods - 2nd cmit

--- home/views.py
+++ home/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# we have to write a decorator this decorator will tell what are the 
+# metds this method will handel
+@api_view(['GET', 'POST'])
+def index(request):
+    courses = {
+        'c_name': 'Python',
+        'learn' : ['flask', 'django', 'flask'],
+        'name': [1,2,3,4,5]
+    }
+    if request.method == 'GET':
+        name = request.GET.get('name')
+        age = request.GET.get('age')
+        lname = request.GET.get('lastName')
+        # print(Response.GET.get('name', 'age', 'lastname'))
+        print(name, age, lname)
+        print("get method")
+        return Response(courses)
+    elif request.method == 'POST':
+        data = request.data
+        print('************')
+        print(data)
+        return Response('data entered successfully')
+       
+    return Response(courses)
+
+''''
+
+Serelizer -> simplest working 
+a seralizer converts the data  into json format 
+
+A class which helps us to seleralize 
+the data from query set to json frmat
+
+'''
